3164de2b7e794a87618e3506e0effd404f983026
ifunanya9 <ifunanyachi.kanu@outlook.com>
2019-08-09 20:55:48 +0100

    python

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello, World!'
+    items = ["Apple", "Pear", "Banana"]
+    output = ""
+    for item in items:
+        output += "<h3>" + item + "</h3>"
+
+    return output
+
+@app.route("/profile")
+def name():
+    paragraph = "<p>My name is Ifunanyachi</p>"
+    return "My profile" + paragraph
