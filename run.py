7d9745bc0e96fbb778870f5db17f4fed29ac7ebc
ifunanya9 <ifunanyachi.kanu@outlook.com>
2019-08-13 00:48:52 +0100

    go

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     items = ["Apple", "Pear", "Banana"]
+    output = render_template("start.html", name="Ifunanyachi Kanu", items=items)
+    print(output)
+    return output
 
-    return render_template("start.html", name="Ifunanyachi Kanu", items=items)
+
 
 @app.route("/profile")
 def name():
     paragraph = "<h4>My name is Ifunanyachi</h4>"
     return "<h1>My profile</h1>" + paragraph
+
+@app.route("/test")
+def test():
+    print(request)
+    return render_template("test.html", name="Ifunanyachi Kanu")
